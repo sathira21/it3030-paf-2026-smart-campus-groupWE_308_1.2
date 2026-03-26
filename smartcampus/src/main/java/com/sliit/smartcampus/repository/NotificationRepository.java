f875a443086d3e2bf32096ca37e7a8102d53eb5e
sathira <IT22547842@my.sliit.lk>
2026-03-26 10:51:11 +0530

    feat: add NotificationController with get and read endpoints

--- smartcampus/src/main/java/com/sliit/smartcampus/repository/NotificationRepository.java
+++ smartcampus/src/main/java/com/sliit/smartcampus/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package com.sliit.smartcampus.repository;
 
 import com.sliit.smartcampus.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserId(Long userId);
 }
