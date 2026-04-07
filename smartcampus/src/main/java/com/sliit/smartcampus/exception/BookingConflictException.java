f6e3b55de50d8c6b10e3b205c07eb33a91ae92e2
pramudiv <pramuvima@gmail.com>
2026-04-07 14:46:47 +0530

    fix: update package declaration for BookingConflictException class

--- smartcampus/src/main/java/com/sliit/smartcampus/exception/BookingConflictException.java
+++ smartcampus/src/main/java/com/sliit/smartcampus/exception/BookingConflictException.java
@@ -1,4 +1,4 @@
-package Backend.exception;
+package com.sliit.smartcampus.exception;
 
 public class BookingConflictException extends RuntimeException {
     public BookingConflictException(String message) {
