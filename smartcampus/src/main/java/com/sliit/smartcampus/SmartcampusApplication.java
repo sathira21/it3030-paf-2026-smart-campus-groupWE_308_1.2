83c1761021aa23eb22af3971f5bcc19bd1d16590
pramudiv <pramuvima@gmail.com>
2026-04-07 16:39:54 +0530

    feat: add Spring Boot application entry point

--- smartcampus/src/main/java/com/sliit/smartcampus/SmartcampusApplication.java
+++ smartcampus/src/main/java/com/sliit/smartcampus/SmartcampusApplication.java
@@ -1,10 +1,9 @@
 package com.sliit.smartcampus;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class SmartcampusApplication {
 
