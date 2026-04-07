756eec56d836cea43076bf6010a32e690bb23aee
pramudiv <pramuvima@gmail.com>
2026-04-07 14:28:27 +0530

    feat: add ResourceNotFoundException class for handling resource not found errors

---
+++ smartcampus/src/main/java/com/sliit/smartcampus/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package Backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
