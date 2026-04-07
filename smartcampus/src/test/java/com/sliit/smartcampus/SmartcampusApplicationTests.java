d6530a3b4afa5830e747edfcd0091aab9359ad0f
pramudiv <pramuvima@gmail.com>
2026-04-07 16:45:00 +0530

    feat: add SmartcampusApplicationTests to verify Spring Boot context loads successfully

--- smartcampus/src/test/java/com/sliit/smartcampus/SmartcampusApplicationTests.java
+++ smartcampus/src/test/java/com/sliit/smartcampus/SmartcampusApplicationTests.java
@@ -1,13 +1,13 @@
 package com.sliit.smartcampus;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SmartcampusApplicationTests {
+@SpringBootTest
+class SmartcampusApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartcampusApplicationTests.class, args);
+	@Test
+	void contextLoads() {
 	}
 
 }
