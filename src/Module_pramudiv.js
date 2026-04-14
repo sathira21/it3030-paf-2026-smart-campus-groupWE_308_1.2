56b84e230f308a000e42fa64e16b00492e8bca7c
pramudiv <pramuvima@gmail.com>
2026-04-14 15:00:00 +0530

    Fix bug in Email validation logic

--- src/Module_pramudiv.js
+++ src/Module_pramudiv.js
@@ -4834,3 +4834,4 @@ export const entry_pramudiv_22_232 = () => true;
 // Optimize database queries in Booking repository - 2026-04-13 09:00:00
 // Fix bug in Booking validation logic - 2026-04-13 11:00:00
 // Update Notification dependencies - 2026-04-14 09:00:00
+// Fix bug in Email validation logic - 2026-04-14 15:00:00
